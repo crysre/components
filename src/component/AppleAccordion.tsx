2045808f001d8b3f00bbbdfb55f5dead72ba13da
Tamojit Hazra <tamosrk@gmail.com>
2026-02-28 06:07:10 +0530

    added images

--- src/component/AppleAccordion.tsx
+++ src/component/AppleAccordion.tsx
@@ -1,5 +1,23 @@
 import { motion } from "motion/react";
 import { useState, type ReactNode } from "react";
+import colors_blue from "../Assets/appleImage/colors_blue__ct0n7mo30vwy_medium.jpg";
+import unibody from "../Assets/appleImage/unibody__eublzdgtajo2_large_2x.jpg";
+import ceramic_shield from "../Assets/appleImage/ceramic_shield__de0653vp43cm_large_2x.jpg";
+import camera_control from "../Assets/appleImage/camera_control__cy5kilwa0kwi_large_2x.jpg";
+import pro_display from "../Assets/appleImage/pro_display__c0jmzc5emcae_large_2x.jpg";
+import vapor_chamber from "../Assets/appleImage/vapor_chamber__b47ohuu3w8eu_large_2x.jpg";
+import action_button from "../Assets/appleImage/viewer_action_button_endframe__d34dse8308uq_large_2x.jpg";
+
+const imagesObj: Record<string, string> = {
+  Colors: colors_blue,
+  "Aluminium unibody": unibody,
+  "Vapour chamber": vapor_chamber,
+  "Ceramic Shield": ceramic_shield,
+  "Immersive pro display": pro_display,
+  "Camera Control": camera_control,
+  "Action button": action_button,
+};
+
 export const AppleAccordion = () => {
   const [open, setOpen] = useState<string | null>(null);
   return (
@@ -7,7 +25,8 @@ export const AppleAccordion = () => {
       <p className="text-white font-bold text-6xl tracking-wide w-450 p-24">
         Take a closer look
       </p>
-      <div className=" relative w-450 h-237.5 rounded-4xl  bg-black  ">
+      <div className=" overflow-hidden relative w-450 h-237.5 rounded-4xl  bg-black  ">
+        <img className="w-full" src={open ? imagesObj[open] : ""} alt="" />
         <div className=" w-106 absolute bottom-40 left-22">
           {accordionArray.map((x, index) => (
             <SpecsCard
@@ -30,36 +49,43 @@ const accordionArray = [
     title: "Colors",
     description:
       "Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.",
+    image: colors_blue,
   },
   {
     title: "Aluminium unibody",
     description:
       "Optimised for performance and battery. Aluminium alloy is remarkably light and has exceptional thermal conductivity.",
+    image: unibody,
   },
   {
     title: "Vapour chamber",
     description:
       "Optimised for performance and battery. Aluminium alloy is remarkably light and has exceptional thermal conductivity.",
+    image: vapor_chamber,
   },
   {
     title: "Ceramic Shield",
     description:
       "Colours. Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.",
+    image: ceramic_shield,
   },
   {
     title: "Immersive pro display",
     description:
       "Colours. Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.",
+    image: pro_display,
   },
   {
     title: "Camera Control",
     description:
       "Colours. Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.",
+    image: camera_control,
   },
   {
     title: "Action button",
     description:
       "Colours. Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.",
+    image: action_button,
   },
 ];
 
@@ -80,7 +106,6 @@ const SpecsCard = ({
 }: SpecsCardInterface) => {
   return open != title ? (
     <motion.div
-      layout
       layoutId={title}
       transition={{
         type: "spring",
@@ -100,7 +125,7 @@ const SpecsCard = ({
         stiffness: 300,
         damping: 25,
       }}
-      onClick={() => setOpen(open === title ? null : title)}
+      //   onClick={() => setOpen(open === title ? null : title)}
       className=" my-2 h-42 w-120    bg-[#1e1e20] rounded-4xl p-8  text-xl text-[#f5f5f7]"
     >
       <span className=" font-bold">{title}.</span> {description}
