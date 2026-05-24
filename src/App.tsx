ae7b8f9eab7856441a2baf80fe85ef005f115560
Tamojit Hazra <tamosrk@gmail.com>
2026-05-24 23:20:11 +0530

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppleAccordion } from "./component/AppleAccordion";
 import { ImageCard } from "./component/ImageCard";
 import { TodoCard } from "./component/TodoCard";
+import { VoiceChat } from "./component/VoiceChat";
 
 function App() {
   return (
     <div className=" font-inter bg-neutral-200 ">
+      <VoiceChat/>
       <AppleAccordion />
       <TodoCard />
       <ImageCard />
---
+++ src/component/VoiceChat.tsx
@@ -0,0 +1,11 @@
+import one from "../Assets/one.jpg";
+export const VoiceChat = () => {
+  return (
+    <div className="flex justify-center items-center h-200 w-full bg-neutral-200">
+        <div className="w-80 h-18 rounded-4xl bg-red-200">
+          <img className="rounded-full h-5" src={one} alt="" />
+        </div>
+    </div> 
+    //https://pin.it/1rUFVLbNo
+  )
+}
