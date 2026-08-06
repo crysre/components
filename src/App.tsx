5caa4aeb4fea6ca56a577857cf79226549f10af6
Tamojit Hazra <tamosrk@gmail.com>
2026-08-07 00:18:22 +0530

    added sliding button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AppleAccordion } from "./component/AppleAccordion";
 import { ImageCard } from "./component/ImageCard";
+import { SlidingButton } from "./component/SlidingButton";
 import { TodoCard } from "./component/TodoCard";
 import { VoiceChat } from "./component/VoiceChat";
 
 function App() {
   return (
     <div className=" font-inter bg-neutral-200 ">
+      <SlidingButton />
       <VoiceChat />
       <AppleAccordion />
       <TodoCard />
